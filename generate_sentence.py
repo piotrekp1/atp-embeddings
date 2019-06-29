c25e4d7fd9fa4f709abcc2911d40681cc7cbf0fb
piotrek <piotrpiekos@gmail.com>
2019-06-29 12:42:04 +0200

    created word2vec style model

--- generate_sentence.py
+++ generate_sentence.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 
 
 def generate_sentence_from_word(df_scores, starting_word, length=10):
     sentence = [starting_word]
+    word = starting_word
     while len(sentence) < length:
         df_node = df_scores[df_scores['prem'] == word]
         if df_node.shape[0] == 0:
@@ -12,7 +14,34 @@ def generate_sentence_from_word(df_scores, starting_word, length=10):
     return sentence
 
 
-def generate_sentence(df_scores, df_scores_start, length=10):
-    initial_distribution = df_scores_start['prem'].value_counts(normalize=True)
+def generate_sentence(df_scores, initial_distribution, length=1000):  # no default length boundaries
     word = np.random.choice(initial_distribution.keys(), p=initial_distribution.values)
     return generate_sentence_from_word(df_scores, word, length)
+
+
+def create_associations(sentence, window_size):
+    associations = []
+    for i, word in enumerate(sentence):
+        for ass_word in sentence[i - window_size:i] + sentence[i+1:i+window_size + 1]:
+            associations.append([word, ass_word])
+    return associations
+
+
+DATASET_SIZE = 10 ** 7
+
+if __name__ == '__main__':
+    df_scores = pd.read_csv('data/prems_mc.csv')
+    init_distr = df_scores['prem'][~df_scores['prem'].isin(df_scores['conj'])].value_counts(normalize=True)
+
+    big_data = []
+    i = 0
+    while len(big_data) < DATASET_SIZE:
+        data = []
+        while len(data) < DATASET_SIZE / 10:
+            sentence = generate_sentence(df_scores, init_distr)
+            data += create_associations(sentence, 3)
+            if i % 200 == 0:
+                print(len(data))
+            i += 1
+        big_data += data
+        pd.DataFrame(big_data).to_csv('data/associations.csv', index=False)
---
+++ train_model.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import os
+
+df = pd.read_csv('data/associations.csv')
+df.rename(columns={'0': 'thm', '1': 'neighbour_thm'}, inplace=True)
+
+vocab = pd.concat([df['thm'], df['neighbour_thm']]).unique()
+encoding = pd.Series(vocab).to_frame()
+enc = encoding.reset_index().rename(columns={'index': 'label'})
+enc = enc.set_index(0)['label']
+df['thm_label'] = df['thm'].map(enc)
+df['neighbour_thm_label'] = df['neighbour_thm'].map(enc)
+
+# definitions
+EMBEDDING_SIZE = 500
+VOCAB_SIZE = vocab.shape[0]
+
+NEGATIVE_SAMPLES = 3
+
+BATCH_SIZE = 32
+EVAL_BATCH_SIZE = 200000
+
+embeddings = tf.Variable(
+    tf.random_uniform([VOCAB_SIZE, EMBEDDING_SIZE], -1.0, 1.0))
+import math
+
+nce_weights = tf.Variable(
+    tf.truncated_normal([VOCAB_SIZE, EMBEDDING_SIZE],
+                        stddev=1.0 / math.sqrt(EMBEDDING_SIZE)))
+nce_biases = tf.Variable(tf.zeros([VOCAB_SIZE]))
+train_inputs = tf.placeholder(tf.int32, shape=[None])
+train_labels = tf.placeholder(tf.int32, shape=[None, 1])
+
+embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+loss = tf.reduce_mean(
+    tf.nn.nce_loss(weights=nce_weights,
+                   biases=nce_biases,
+                   labels=train_labels,
+                   inputs=embed,
+                   num_sampled=NEGATIVE_SAMPLES,
+                   num_classes=VOCAB_SIZE))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.000025).minimize(loss)
+
+
+# training
+
+
+def whole_set(method='skip_gram'):
+    if method == 'skip_gram':
+        inputs = df['thm_label'].values
+        labels = df['neighbour_thm_label'].values.reshape(-1, 1)
+    elif method == 'CBOW':
+        inputs = df['neighbour_thm_label'].values
+        labels = df['thm_label'].values.reshape(-1, 1)
+    else:
+        raise Exception('Wrong training method')
+    return inputs, labels
+
+
+all_inputs, all_labels = whole_set()
+
+
+def generate_batch():
+    """
+    Generates minibatch of size BATCH_SIZE for training accordingly to the method ('skip_gram' or 'CBOW').
+    Returns:
+        inputs - vector of inputs for training
+        labels - vector of labels for training
+    """
+
+    for epoch_num in range(10000):
+        for batch_lower_bound in range(0, df.shape[0], BATCH_SIZE):
+            batch_upper_bound = batch_lower_bound + BATCH_SIZE
+            yield (all_inputs[batch_lower_bound: batch_upper_bound], all_labels[batch_lower_bound:batch_upper_bound])
+
+
+# Summaries
+with tf.name_scope('Performance'):
+    BATCH_LOSS_PH = tf.placeholder(tf.float32, shape=None, name='loss_summary')
+    BATCH_LOSS_SUMMARY = tf.summary.scalar('loss', BATCH_LOSS_PH)
+    LOSS_PH = tf.placeholder(tf.float32, shape=None, name='loss_summary')
+    LOSS_SUMMARY = tf.summary.scalar('loss', LOSS_PH)
+PERFORMANCE_SUMMARIES = tf.summary.merge([BATCH_LOSS_SUMMARY, LOSS_SUMMARY])
+
+SUMMARIES = 'summaries'
+RUNID = f'run_{1}'
+SUMM_WRITER = tf.summary.FileWriter(os.path.join(SUMMARIES, RUNID))
+
+saver = tf.train.Saver()
+
+conf = tf.ConfigProto(log_device_placement=True)
+
+conf.gpu_options.allow_growth = True
+with tf.Session(config=conf) as sess:
+    sess.run(tf.global_variables_initializer())
+    for i, (inputs, labels) in enumerate(generate_batch()):
+        feed_dict = {train_inputs: inputs, train_labels: labels}
+        _, cur_loss = sess.run([optimizer, loss], feed_dict=feed_dict)
+        summ_loss = sess.run(BATCH_LOSS_SUMMARY, feed_dict={BATCH_LOSS_PH: cur_loss})
+        SUMM_WRITER.add_summary(summ_loss, i)
+        if i % 2000 == 0:
+            inds = np.random.choice(all_inputs.shape[0], EVAL_BATCH_SIZE)
+            feed_dict = {train_inputs: all_inputs[inds], train_labels: all_labels[inds]}
+            cur_loss = sess.run(loss, feed_dict=feed_dict)
+            tf.summary.scalar('loss', cur_loss)
+
+            summ_loss = sess.run(LOSS_SUMMARY, feed_dict={LOSS_PH:cur_loss})
+            SUMM_WRITER.add_summary(summ_loss, i)
+
+            print(i, cur_loss)
+            if i % 100000 == 0 and i > 0:
+                save_path = saver.save(sess, "models/model3.ckpt")
